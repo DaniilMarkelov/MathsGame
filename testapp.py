2e741ffdaeee762a02755ed9d63f4ba307856594
Daniil Markelov <daniil@markelov.org>
2017-12-09 14:54:25 +0000

    Application goes back to the main App loop now
    Before this commit our application would not offer the next
    problem to solve. It gave you one and told you if the answer
    was correct or wrong and didn't offer the next problem.
    
    Now it gives you a problem to solve, tells you if the answer is correct
    or wrong and after a few seconds it gives you the next problem.
    
    To exit from the application, you need to close the window.
    
    Added new bug in the process - correct answer is always on the third
    button.

--- testapp.py
+++ testapp.py
@@ -7,16 +7,22 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
+from functools import partial
+from kivy.properties import ObjectProperty
+from kivy.clock import Clock
 
+layout = FloatLayout(size=(300, 300)) 
 
 class TestApp(App):
+    current_app = ObjectProperty(None)
+
     def build(self):
-                
-        layout = FloatLayout(size=(300, 300))
-        
+        self.current_app = TestApp()
+        layout.clear_widgets()
+
         no1 = random.randint(1, 10)
         no2 = random.randint(1, 10)
-        answer = no1 + no2
+        answer = no1 + no2   
 
         Sum = str(no1) + "+" + str(no2) + "="
         label = Label(text= Sum,
@@ -24,95 +30,70 @@ class TestApp(App):
                       font_size='100sp')
         layout.add_widget(label)
 
-        def correct(instance):
-            layout.clear_widgets()
-            correct = Label(text= 'Correct!',
-                          pos= (5, 20),
-                          font_size='200sp')
-            layout.add_widget(correct)
-
-        def wrong(instance):
-            layout.clear_widgets()
-            wrong = Label(text= 'Wrong!',
-                          pos= (5, 20),
-                          font_size='200sp')
-            layout.add_widget(wrong)
-
         random_button = random.randint(1, 3)
 
-        if random_button == 1:
-
-            b1 = Button(text= str(answer),
-                        size_hint= [.33, .3],
-                        pos= (0, 0),
-                        font_size='75sp',
-                        on_press= correct)
-            layout.add_widget(b1)
+        button_1 = Button(text= str(answer + random.randint(1, 5)),
+                          size_hint= [.33, .3],
+                          pos= (0, 0),
+                          font_size='75sp',
+                          on_press=partial(self.correction, answer = False))
+        layout.add_widget(button_1)
+    
+        button_2 = Button(text= str(answer - random.randint(1, 5)),
+                          size_hint= [.33, .3],
+                          pos= (267.5, 0),
+                          font_size='75sp',
+                          on_press=partial(self.correction, answer =False))
+        layout.add_widget(button_2)
         
-            b2 = Button(text= str(answer + random.randint(1, 5)),
-                        size_hint= [.33, .3],
-                        pos= (267.5, 0),
-                        font_size='75sp',
-                        on_press= wrong)
-            layout.add_widget(b2)
-            
-            b3 = Button(text= str(answer - random.randint(1, 5)),
-                        size_hint= [.33, .3],
-                        pos= (535, 0),
-                        font_size='75sp',
-                        on_press= wrong)
-            layout.add_widget(b3)
-            return layout
+        button_3 = Button(text= str(answer),
+                          size_hint= [.33, .3],
+                          pos= (535, 0),
+                          font_size='75sp',
+                          on_press=partial(self.correction, answer = True))
+        layout.add_widget(button_3)
 
-        if random_button == 2:
+        return layout
 
-            b1 = Button(text= str(answer - random.randint(1, 5)),
-                        size_hint= [.33, .3],
-                        pos= (0, 0),
-                        font_size='75sp',
-                        on_press= wrong)
-            layout.add_widget(b1)
-        
-            b2 = Button(text= str(answer),
-                        size_hint= [.33, .3],
-                        pos= (267.5, 0),
-                        font_size='75sp',
-                        on_press= correct)
-            layout.add_widget(b2)
-            
-            b3 = Button(text= str(answer + random.randint(1, 5)),
-                        size_hint= [.33, .3],
-                        pos= (535, 0),
-                        font_size='75sp',
-                        on_press= wrong)
-            layout.add_widget(b3)
-            return layout
+    def correction(self, event, answer):
+        if self.current_app:
+            self.current_app.stop()
+        if answer==True:
+            self.current_app = Correct()
+        else:
+            self.current_app = Wrong()
+        self.current_app.run()
 
-        if random_button == 3:
+class Correct(App):
+    def build(self):
+        self.current_app = TestApp()
+        layout.clear_widgets()
+        Clock.schedule_once(self.loop, 1)
+        correct = Label(text= 'Correct!',
+                        pos= (5, 20),
+                        font_size='200sp')
+        layout.add_widget(correct)
+        return layout
 
-            b1 = Button(text= str(answer + random.randint(1, 5)),
-                        size_hint= [.33, .3],
-                        pos= (0, 0),
-                        font_size='75sp',
-                        on_press= wrong)
-            layout.add_widget(b1)
-        
-            b2 = Button(text= str(answer - random.randint(1, 5)),
-                        size_hint= [.33, .3],
-                        pos= (267.5, 0),
-                        font_size='75sp',
-                        on_press= wrong)
-            layout.add_widget(b2)
-            
-            b3 = Button(text= str(answer),
-                        size_hint= [.33, .3],
-                        pos= (535, 0),
-                        font_size='75sp',
-                        on_press= correct)
-            layout.add_widget(b3)
-            return layout
+    def loop(self, dt):
+        layout.clear_widgets()
+        self.current_app.run()
+ 
+class Wrong(App):
+    def build(self):
+        self.current_app = TestApp()
+        layout.clear_widgets()
+        Clock.schedule_once(self.loop, 1)
+        wrong = Label(text= 'Wrong!',
+                      pos= (5, 20),
+                      font_size='200sp')
+        layout.add_widget(wrong)
+        return layout
 
-while True:
-    TestApp().run()
-    time.sleep(.5)
+    def loop(self, dt):
+        layout.clear_widgets()
+        self.current_app.run()
 
+
+app = TestApp()
+app.run()
